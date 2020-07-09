70e9d70ed8ca6d8a7a22d00a8047c567f6d3f6be
salsbeeltareqq <beelootareqq1313@icloud.com>
2020-07-09 16:56:46 +0300

    mul solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -32,12 +32,21 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(c, d) { 
 
-}
+
+        var mul = c * d;
+    
+        var array2 = [mul, 'The product of ' + c + ' and ' + d + ' is ' + mul +'.'];
+        return array2;
+        // "The product of 5 and 9 is 45."
+
+    }
+
+
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
