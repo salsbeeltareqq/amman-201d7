0a5102fe31737336411da7b7c94c67b4043c19f5
salsbeeltareqq <beelootareqq1313@icloud.com>
2020-07-11 23:11:30 +0300

    summulsolved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -62,12 +62,27 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-
-}
+function sumAndMultiply(a, b, c) {
+    var sum_a_b = sum(a, b)[0];
+    var totalSum = sum(sum_a_b, c)[0];
+    var mult_a_b = multiply(a, b)[0];
+    var totalMult = multiply(mult_a_b, c)[0];
+    var sumMsg = a + " and " + b + " and " + c + " sum to " + totalSum + ".";
+    var multiMsg =
+      "The product of " +
+      a +
+      " and " +
+      b +
+      " and " +
+      c +
+      " is " +
+      totalMult +
+      ".";
+    return [totalSum, totalMult, sumMsg, multiMsg];
+  }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
